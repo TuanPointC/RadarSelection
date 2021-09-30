753e39c688475615b2d2a51a2d883133fc61aa3f
TuanPointC <tuan.nv19010040@st.phenikaa-uni.edu.vn>
2021-09-30 22:47:23 +0700

    lan2

---
+++ dql/dql.py
@@ -0,0 +1,95 @@
+import numpy as np
+import tensorflow as tf
+
+# Backbone Architecture
+
+
+class Backbone:
+
+    def __init__(self, out_dims):
+
+        self.layers = [
+            tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
+            tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
+            tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
+            tf.keras.layers.Dense(units=out_dims)
+        ]
+
+    def __call__(self, inputs):
+        # Call Function
+        x = inputs
+        outputs = self.call(inputs)
+        return outputs
+
+    def call(self, inputs):
+        # Perform
+        x = inputs
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class DQL:
+    def __init__(self, input_dims, num_actions, backbone):
+        """
+        Initial Method for Deep Q Learning Method
+        params: input_dims: Integer
+        params: num_actions: Integer
+        returns: None
+        """
+        self.input_dims = input_dims
+        self.num_actions = num_actions
+        self.backbone = backbone
+        self.states = tf.compat.v1.placeholder(dtype=tf.float32,
+                                               shape=(None, self.input_dims))
+        self.next_states = tf.compat.v1.placeholder(dtype=tf.float32,
+                                                    shape=(None, self.input_dims))
+        self.actions = tf.compat.v1.placeholder(dtype=tf.int32, shape=(None,))
+        self.rewards = tf.compat.v1.placeholder(
+            dtype=tf.float32, shape=(None,))
+        self.dones = tf.compat.v1.placeholder(dtype=tf.float32, shape=(None, ))
+        self.gamma = tf.compat.v1.placeholder(dtype=tf.float32, shape=None)
+        self.lr = tf.compat.v1.placeholder(dtype=tf.float32, shape=None)
+
+        self._define_model()
+
+    def _define_model(self):
+        # Create the Deep Q Learning agent
+        self.agent = self.backbone(out_dims=self.num_actions)
+        # convert action to one-hot format
+        one_hot_actions = tf.one_hot(self.actions, self.num_actions)
+
+        self.q_values = self.agent(self.states)
+        q_action_values = tf.reduce_sum(
+            one_hot_actions * self.q_values, axis=-1)
+        # Using Bellman Equation to Caculate Ground-Truth
+        q_next_values = self.agent(self.next_states)
+        q_next_values = tf.reduce_max(q_next_values, axis=-1)
+        label = (q_next_values * (1.0 - self.dones)) * \
+            self.gamma + self.rewards
+        # define Objective Function
+        self.loss = tf.losses.mean_squared_error(label, q_action_values)
+        self.opt = tf.compat.v1.train.AdamOptimizer(
+            self.lr).minimize(loss=self.loss)
+
+    def sel_action(env, model, sess, state, epsilon):
+        """
+        perform selecting action for Deep Q Learning Method
+
+        """
+        if np.random.random() < epsilon:
+            action = env.sam_action()
+            return action
+        else:
+            state = np.expand_dims(state, axis=0)
+            # Run Inference
+            [q_values] = sess.run([model.q_values],
+                                  feed_dict={model.states: state})
+            q_values = q_values[0]
+            act_idx = np.argmax(q_values)
+            action = env.index2action(act_idx)
+            return action
+
+
+if __name__ == "__main__":
+    model = DQL(input_dims=8, num_actions=10, backbone=Backbone)
--- dql/env.py
+++ dql/env.py
@@ -1,5 +1,8 @@
+import math
+
+
 class Transmitting_Radar:
-    def __init__(self,x_m,y_m,p_m,beta,a):
+    def __init__(self, x_m, y_m, p_m, beta, a):
         """
         params - x_m: float - X coordinate
         params - y_m: float - Y coordinate
@@ -7,25 +10,49 @@ class Transmitting_Radar:
         params - beta  : float - bandwidth
         params - a  : int - Is chosen radar
         """
-        if(p_m<0 and beta<0):
-            raise Exception("Initial Values for Transmitting radar must be Positive!")
-        
-        self.x_m=x_m
-        self.y_m=y_m
-        self.p_m=p_m
-        self.beta=beta
-        self.a=a
+        if(p_m < 0 and beta < 0):
+            raise Exception(
+                "Initial Values for Transmitting radar must be Positive!")
+
+        self.x_m = x_m
+        self.y_m = y_m
+        self.p_m = p_m
+        self.beta = beta
+        self.a = a
+
 
 class Receiving_Radar:
-    def __init__(self,x_n,y_n,b):
+    def __init__(self, x_n, y_n, b):
         """
         params - x_n: float - X coordinate
         params - y_n: float - Y coordinate
         params - p_n: float - transmit power
         params - a  : int - Is chosen radar
         """
-    
-        self.x_m=x_n
-        self.y_m=y_n
-        self.b=b
 
+        self.x_m = x_n
+        self.y_m = y_n
+        self.b = b
+
+
+class FunctionTest:
+
+    def distance(self,position_radar, position_target):
+        return math.sqrt((position_target.x-position_radar.x)**2+(position_target.y-position_radar.y)**2)
+
+    def loss_efficients(self,position_target,position_tr,position_rr):
+        distance_tr=self.distance(position_tr,position_target)
+        distance_rr=self.distance(position_rr,position_target)
+        return 1/(distance_rr*distance_tr)**2
+    
+    def e_m(bandwidth):
+        c=3*10**8
+        noise_2=-110
+        return (8*math.pi**2*bandwidth**2)/(noise_2*c**2)
+    
+
+    
+    def o_mn(self,power,h,bandwidth):
+        return self.loss_efficients()*power*self.e_m(bandwidth)*h**2*()
+
+    #def TRC(action, position_tr, position_rr, position_target, m, n):
